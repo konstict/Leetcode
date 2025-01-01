6f66363179b6203028514cedaf8dc4a31331dde8
kostik <97781288+konstict@users.noreply.github.com>
2025-01-01 22:12:56 +0300

    Create 20. easy Valid Parentheses.cpp

---
+++ easy/20. easy Valid Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> a;
+        for(char i : s){
+            if(i == '(' or i == '[' or i == '{'){
+                a.push(i);
+            }
+            else if(a.empty()){return false;}
+            else if( (i == ')' and a.top() == '(') 
+                or (i == ']' and a.top() == '[') 
+                or (i == '}' and a.top() == '{') ){
+                a.pop();
+                }
+            else{return false;}
+        }
+        if(a.empty()){
+            return true;
+        }
+        return false;;
+    }
+};//0ms, 16 min, O(n+3)
